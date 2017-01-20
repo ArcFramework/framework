51b212e72bb7227c2d0defb8285073eb3f157581
Andrew Feeney <webspannerdev@gmail.com>
2017-01-20 18:04:26 +1100

    Implement some basic Eloquent Models

---
+++ src/Arc/Models/PostMeta.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Arc\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PostMeta extends Model
+{
+    public $timestamps = false;
+
+    protected $guarded = [];
+}
+
+
---
+++ src/Arc/Models/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Arc\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    public $timestamps = false;
+
+    protected $guarded = [];
+}
+

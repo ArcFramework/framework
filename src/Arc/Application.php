f549a75b09699b3946820d224101c29d1ca44972
Andrew Feeney <webspannerdev@gmail.com>
2017-01-26 21:06:42 +1100

    Remove this constants since we’ve introduced environment variables

--- src/Arc/Application.php
+++ src/Arc/Application.php
@@ -6,16 +6,7 @@ use Illuminate\Container\Container;
 
 class Application extends Container
 {
-    public $pluginDirectory;
-    public $pluginFileName;
-    public $pluginPath;
-    public $pluginNamespace;
 
-    public function __construct($pluginFileName = null, $pluginNamespace = null)
-    {
-        $this->pluginFilename = $pluginFileName;
-        $this->pluginDirectory = dirname($pluginFileName);
-        $this->pluginPath = plugins_url(null, $this->pluginFileName);
-        $this->pluginNamespace = $pluginNamespace;
-    }
+
 }
+

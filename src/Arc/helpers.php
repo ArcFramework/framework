3b942fd5ecb1021454c22540f239afde71395209
Andrew Feeney <webspannerdev@gmail.com>
2017-01-26 21:07:24 +1100

    Add env() and config() helpers

--- src/Arc/helpers.php
+++ src/Arc/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Container\Container;
+use Illuminate\Support\Str;
 
 if (! function_exists('app')) {
     /**
@@ -16,5 +17,62 @@ if (! function_exists('app')) {
             return Container::getInstance();
         }
         return Container::getInstance()->make($make, $parameters);
+    }
+}
+
+if (! function_exists('config')) {
+    /**
+     * Get / set the specified configuration value.
+     *
+     * If an array is passed as the key, we will assume you want to set an array of values.
+     *
+     * @param  array|string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function config($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('config');
+        }
+        if (is_array($key)) {
+            return app('config')->set($key);
+        }
+        return app('config')->get($key, $default);
+    }
+}
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+        if ($value === false) {
+            return value($default);
+        }
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+        if (strlen($value) > 1 && Str::startsWith($value, '"') && Str::endsWith($value, '"')) {
+            return substr($value, 1, -1);
+        }
+        return $value;
     }
 }

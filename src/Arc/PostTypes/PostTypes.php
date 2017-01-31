7f38df9dea85e499309ebdd0b115609bfc134ba5
Andrew Feeney <webspannerdev@gmail.com>
2017-01-31 17:39:28 +1100

    Add extra custom post type declaration functionality

--- src/Arc/PostTypes/PostTypes.php
+++ src/Arc/PostTypes/PostTypes.php
@@ -2,16 +2,42 @@
 
 namespace Arc\PostTypes;
 
+use Arc\Http\Controllers\ControllerHandler;
+use Arc\View\Builder;
+
 class PostTypes
 {
-    private $name;
-    private $pluralName;
-    private $public;
-    private $slug;
+    protected $controllerMethod;
+    protected $controllerHandler;
+    protected $metaBoxes;
+    protected $name;
+    protected $pluralName;
+    protected $public;
+    protected $slug;
+    protected $supports;
+    protected $view;
+
+    public function __construct(Builder $viewBuilder, ControllerHandler $controllerHandler)
+    {
+        $this->controllerHandler = $controllerHandler;
+        $this->viewBuilder = $viewBuilder;
+    }
 
     public function createPublic()
     {
         $this->public = true;
+        return $this;
+    }
+
+    public function whichSupportsFields($fields)
+    {
+        $this->supports = $fields;
+        return $this;
+    }
+
+    public function withMetaBoxes($metaBoxes)
+    {
+        $this->metaBoxes = $metaBoxes;
         return $this;
     }
 
@@ -23,9 +49,46 @@ class PostTypes
                 'labels' => [
                     'name' => $this->name,
                     'plural' => $this->pluralName,
-                ]
+                ],
+                'supports' => $this->supports ?? ['title', 'editor'],
             ]);
         });
+
+        if (!is_null($this->metaBoxes)) {
+            add_action('load-post.php', [$this, 'setupMetaBoxes']);
+            add_action('load-post-new.php', [$this, 'setupMetaBoxes']);
+        }
+
+        // Register the template handler for a controller method
+        if (!is_null($this->controllerMethod)) {
+            app()->bind($this->slug, function() {
+                return $this->controllerHandler->parseControllerCall($this->controllerMethod);
+            });
+           return $this->registerTemplateHandler();
+        }
+
+        // Register the template handler for a view
+        if (!is_null($this->view)) {
+            app()->bind($this->slug, function() {
+                return $this->viewBuilder->build($this->view, ['post' => get_post()]);
+            });
+            return $this->registerTemplateHandler();
+        }
+    }
+
+    public function registerTemplateHandler()
+    {
+        add_filter('single_template', function() {
+            global $post;
+            if ($post->post_type == $this->slug) {
+                return config('plugin_path') . 'custom_post_type.php';
+            }
+        });
+    }
+
+    public function render($postType)
+    {
+        echo app($postType);
     }
 
     public function withPluralName($pluralName)
@@ -44,5 +107,32 @@ class PostTypes
     {
         $this->slug = $slug;
         return $this;
+    }
+
+    public function whichCallsControllerMethod($controllerMethod)
+    {
+        $this->controllerMethod = $controllerMethod;
+        return $this;
+    }
+
+    public function whichDisplaysView($view)
+    {
+        $this->view = $view;
+        return $this;
+    }
+
+    public function setupMetaBoxes()
+    {
+        foreach ($this->metaBoxes as $metaBox) {
+            add_meta_box(
+                config('plugin_slug') . '-' . $metaBox['title'] . '-meta-box',
+                $metaBox['title'],
+                $metaBox['callback'],
+                $this->slug,
+                $metaBox['context'] ?? 'side',
+                $metaBox['priority'] ?? 'default',
+                $metaBox['callbackArguments'] ?? null
+            );
+        }
     }
 }

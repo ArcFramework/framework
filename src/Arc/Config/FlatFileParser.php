0da17662fec0caf661974706b9a214f20c414e02
Andrew Feeney <webspannerdev@gmail.com>
2017-01-31 17:36:23 +1100

    Add parseDirectory method

--- src/Arc/Config/FlatFileParser.php
+++ src/Arc/Config/FlatFileParser.php
@@ -3,9 +3,15 @@
 namespace Arc\Config;
 
 use Arc\Application;
+use Arc\FileSystem\FileManager;
 
 class FlatFileParser
 {
+    public function __construct(FileManager $fileManager)
+    {
+        $this->fileManager = $fileManager;
+    }
+
     /**
      * Requires the given config file, passing in the given variables and returns the result
      *
@@ -26,5 +32,16 @@ class FlatFileParser
         }
 
         return include($fileName);
+    }
+
+    public function parseDirectory($directoryName, $variables = [])
+    {
+        foreach($variables as $name => $value) {
+            $$name = $value;
+        }
+
+        foreach($this->fileManager->getAllFilesInDirectory(config('plugin_path') . '/' . $directoryName) as $file) {
+            include ($file->getPath() . '/' . $file->getFilename());
+        }
     }
 }

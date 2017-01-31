dead01c0095499f1f7d1a1de2203911fb048e4ea
Andrew Feeney <webspannerdev@gmail.com>
2017-01-31 17:41:50 +1100

    Add Test for Filemanager class

---
+++ tests/FileSystem/FileManagerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use MakeWeb\REAXMLImporter\FileSystem;
+
+use MakeWeb\REAXMLImporter\FileSystem\File;
+use MakeWeb\REAXMLImporter\FileSystem\FileManager;
+
+class FileManagerTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->fileManager = new FileManager;
+    }
+
+    /** @test */
+    public function the_get_all_files_in_directory_method_returns_all_files_as_file_objects()
+    {
+        $testDirectory = '/tmp/filesystem_test';
+
+        if (file_exists($testDirectory)) {
+            $this->fileManager->deleteDirectory($testDirectory);
+        }
+
+        mkdir($testDirectory);
+
+        $testFile = "$testDirectory/testFile.txt";
+        $fh = fopen($testFile, 'w');
+        fwrite($fh, 'text');
+        fclose($fh);
+
+        $files = $this->fileManager->getAllFilesInDirectory($testDirectory);
+
+        $this->assertEquals(1, count($files));
+        $this->assertTrue(array_pop($files) instanceof File);
+    }
+
+    /** @test */
+    public function the_create_fresh_directory_method_creates_a_directory_if_it_does_not_exist()
+    {
+        $this->fileManager->deleteDirectory('/tmp/fresh');
+
+        $this->assertFalse(file_exists('/tmp/fresh'));
+        $this->fileManager->createFreshDirectory('/tmp/fresh');
+        $this->assertTrue(file_exists('/tmp/fresh'));
+    }
+}

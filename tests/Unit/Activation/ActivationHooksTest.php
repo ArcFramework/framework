ec48cd5e24abb15f913035cc5be5f49713d7aef7
Andrew Feeney <webspannerdev@gmail.com>
2017-03-17 07:34:31 +1100

    Fix issue where test relied an actual plugin with an actual config file existing

--- tests/Unit/Activation/ActivationHooksTest.php
+++ tests/Unit/Activation/ActivationHooksTest.php
@@ -1,18 +1,23 @@
 <?php
 
 use Arc\Activation\ActivationHooks;
+use Arc\Config\Config;
 
 class ActivationHooksTest extends FrameworkTestCase
 {
     /** @test */
     public function an_activation_hook_can_be_registered()
     {
+        app('configuration')->useTestConfig([
+            'plugin_filename' => 'test-plugin-filename.php'
+        ]);
+
         $doThis = ['object' => 'method'];
 
         WP_Mock::wpFunction('register_activation_hook', [
             'times' => 1,
             'args' => [
-                config('plugin_filename'),
+                'test-plugin-filename.php',
                 $doThis
             ]
         ]);
@@ -23,12 +28,16 @@ class ActivationHooksTest extends FrameworkTestCase
     /** @test */
     public function a_deactivation_hook_can_be_registered()
     {
+        app('configuration')->useTestConfig([
+            'plugin_filename' => 'test-plugin-filename.php'
+        ]);
+
         $doThis = ['object' => 'method'];
 
         WP_Mock::wpFunction('register_deactivation_hook', [
             'times' => 1,
             'args' => [
-                config('plugin_filename'),
+                'test-plugin-filename.php',
                 $doThis
             ]
         ]);

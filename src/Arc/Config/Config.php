1aa99e0611c88ffd935a29cbe293296390438ec0
Andrew Feeney <webspannerdev@gmail.com>
2017-03-17 07:33:42 +1100

    Add useTestConfig method to allow easy insertion of dummy config values for testing

--- src/Arc/Config/Config.php
+++ src/Arc/Config/Config.php
@@ -4,9 +4,21 @@ namespace Arc\Config;
 
 class Config
 {
+    protected $testConfig;
+
+    public function useTestConfig($testConfig)
+    {
+        $this->testConfig = $testConfig;
+    }
+
     public function get($key)
     {
         $pluginPath = env('PLUGIN_PATH', ABSPATH . 'wp-content/plugins/' . env('PLUGIN_SLUG') . '/');
+
+        // If a test config value has been set for the given key we'll use that
+        if (isset($this->testConfig[$key])) {
+            return $this->testConfig[$key];
+        }
 
         $configValues = include($pluginPath . 'config/app.php');
 

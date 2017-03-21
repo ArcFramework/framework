e98b0e22ef2e6f23ed6dd481cf8231f39b00add1
Andrew Feeney <webspannerdev@gmail.com>
2017-03-22 06:27:37 +1100

    Add nice little facility for mocking global functions

--- tests/FrameworkTestCase.php
+++ tests/FrameworkTestCase.php
@@ -2,9 +2,13 @@
 
 abstract class FrameworkTestCase extends PHPUnit_Framework_TestCase
 {
+    public static $functions;
+
     public function setUp()
     {
         WP_Mock::setUp();
+
+        self::$functions = Mockery::mock();
 
         $this->plugin = new Arc\BasePlugin(__FILE__);
         $this->app = $this->plugin->app;

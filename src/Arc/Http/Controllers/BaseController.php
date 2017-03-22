fa0a2aa9a3935201167e8ecf75cf4ab4df2a6608
Andrew Feeney <webspannerdev@gmail.com>
2017-03-22 12:22:09 +1100

    Remove call to non-existent app function

--- src/Arc/Http/Controllers/BaseController.php
+++ src/Arc/Http/Controllers/BaseController.php
@@ -7,11 +7,16 @@ use Arc\Http\ValidatesRequests;
 
 class BaseController
 {
-    private $plugin;
+    public $plugin;
     private $validator;
 
     public function validate($request, $rules)
     {
-        app()->make(ValidatesRequests::class)->validate($request, $rules);
+        $this->plugin->make(ValidatesRequests::class)->validate($request, $rules);
+    }
+
+    public function setPluginInstance(BasePlugin $plugin)
+    {
+        $this->plugin = $plugin;
     }
 }

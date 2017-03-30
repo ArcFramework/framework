93f9f1396786eb7de5e2c1a557829498c2f4575d
Andrew Feeney <webspannerdev@gmail.com>
2017-03-30 12:13:51 +1100

    Implement tests for Mailer class regarding handling from addresses

--- tests/Unit/Mail/MailerTest.php
+++ tests/Unit/Mail/MailerTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use Arc\Config\WPOptions;
+use Arc\Hooks\Actions;
+use Arc\Hooks\Filters;
 use Arc\Mail\Email;
 use Arc\Mail\Mailer;
 use Arc\View\Builder;
@@ -16,6 +19,7 @@ class MailerTest extends FrameworkTestCase
             ->withMessage('<span class="red"> Test message. </span>')
             ->withCSS('.red { color: red; }')
             ->to('test@domain.com')
+            ->from('from@domain.com')
             ->withSubject('Test Subject');
 
         $viewBuilder = Mockery::mock('Arc\View\Builder');
@@ -23,7 +27,7 @@ class MailerTest extends FrameworkTestCase
             ->once()
             ->andReturn('Rendered view');
 
-        $this->plugin->instance(Builder::class, $viewBuilder);
+        $this->app->instance(Builder::class, $viewBuilder);
 
         WP_Mock::wpFunction('wp_mail', [
             'times' => 1,
@@ -38,8 +42,46 @@ class MailerTest extends FrameworkTestCase
             ]
         ]);
 
-        $mailer = $this->plugin->make(Mailer::class);
+        $mailer = $this->app->make(Mailer::class);
         $mailer->send($email);
     }
-}
 
+    /** @test */
+    public function send_method_uses_default_wp_address_for_email_without_from_address()
+    {
+        $wpOptions = $this->app->make(WPOptions::class);
+        $wpOptions->setTest('admin_email', 'admin@domain.com');
+        $wpOptions->setTest('wp_mail_from', 'from@domain.com');
+
+        $email = (new Email)
+            ->withMessage('Test message.')
+            ->to('test@domain.com');
+
+        $this->app->instance(WPOptions::class, $wpOptions);
+
+        $mailer = $this->app->make(Mailer::class);
+        $mailer->send($email);
+
+        // TODO Verify that the default address is actually being used
+    }
+
+    /** @test */
+    public function get_from_method_returns_default_wp_address_for_email_without_from_address()
+    {
+        $wpOptions = $this->app->make(WPOptions::class);
+        $wpOptions->setTest('admin_email', 'admin@domain.com');
+
+        \WP_Mock::onFilter('wp_mail_from')
+            ->with('')
+            ->reply('from@domain.com');
+
+        $this->app->instance(WPOptions::class, $wpOptions);
+
+        $email = (new Email)
+            ->withMessage('Test message.')
+            ->to('test@domain.com');
+
+        $mailer = $this->app->make(Mailer::class);
+        $this->assertEquals('from@domain.com', $mailer->getFromAddress($email));
+    }
+}

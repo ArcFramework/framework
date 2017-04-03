76117ada1e10afefc9b9fe5244b6bade5fa3c82f
Andrew Feeney <webspannerdev@gmail.com>
2017-04-03 14:04:07 +1000

    Changes to BaseController
    - Change the way we bind the app object to constructor as per ControllerDispatcher object
    - Add response and redirect methods to mimic Laravel’s response and redirect helpers (We can’t use these as they might conflict)

--- src/Arc/Http/Controllers/BaseController.php
+++ src/Arc/Http/Controllers/BaseController.php
@@ -4,26 +4,49 @@ namespace Arc\Http\Controllers;
 
 use Arc\BasePlugin;
 use Arc\Http\ValidatesRequests;
-
-use Arc\Routing\RequestFactory;
+use Illuminate\Routing\Redirector;
+use Illuminate\Routing\ResponseFactory;
 
 class BaseController
 {
-    public $plugin;
+    public $app;
     private $validator;
+
+    public function __construct(BasePlugin $app)
+    {
+        $this->app = $app;
+    }
 
     public function validate($request, $rules)
     {
-        $this->plugin->make(ValidatesRequests::class)->validate($request, $rules);
+        $this->app->make(ValidatesRequests::class)->validate($request, $rules);
     }
 
-    public function setPluginInstance(BasePlugin $plugin)
+    public function response($content = null, $status = null, $headers = null)
     {
-        $this->plugin = $plugin;
+        $factory = $this->app->make(ResponseFactory::class);
+
+        if (func_num_args() === 0) {
+            return $factory;
+        }
+
+        return $factory->make($content, $status, $headers);
     }
 
-    public function response()
+    /**
+     * Get an instance of the redirector.
+     *
+     * @param  string|null  $to
+     * @param  int     $status
+     * @param  array   $headers
+     * @param  bool    $secure
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
+    public function redirect($to = null, $status = 302, $headers = [], $secure = null)
     {
-        return $this->plugin->make(RequestFactory::class);
+        if (is_null($to)) {
+            return $this->app->make(Redirector::class);
+        }
+        return $this->app->make(Redirector::class)->to($to, $status, $headers, $secure);
     }
 }

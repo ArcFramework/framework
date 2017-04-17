1e0f0dc9cc90094a2730c4d420f316022369a31a
Andrew Feeney <webspannerdev@gmail.com>
2017-04-17 13:08:51 +1000

    Add findByEmail method to user model

--- src/Arc/Models/User.php
+++ src/Arc/Models/User.php
@@ -9,5 +9,15 @@ class User extends Model
     public $timestamps = false;
 
     protected $guarded = [];
+
+    /**
+     * Returns the user matching the given email address or null if none exists
+     * @param string $email
+     * @return \Arc\Models\User|null
+     **/
+    public static function findByEmail($email)
+    {
+        return self::whereUserEmail($email)->first();
+    }
 }
 

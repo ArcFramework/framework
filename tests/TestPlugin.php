1a21b0e57d0449841829b0b319cca47c5ac75e73
Andrew Feeney <webspannerdev@gmail.com>
2017-04-20 21:33:24 +1000

    Implement abstract methods on TestPlugin class

--- tests/TestPlugin.php
+++ tests/TestPlugin.php
@@ -1,8 +1,29 @@
 <?php
 
-use Arc\BasePlugin;
+use Arc\Application;
 
-class TestPlugin extends BasePlugin
+class TestPlugin extends Application
 {
+    protected static $instance;
 
+    /**
+     * Set the shared instance of the application.
+     *
+     * @param  Application|null  $container
+     * @return static
+     */
+    public static function setApplicationInstance(Application $application)
+    {
+        self::$instance = $application;
+    }
+
+    /**
+     * Get the shared instance of the application.
+     *
+     * @return static
+     */
+    public static function app()
+    {
+        return self::$instance;
+    }
 }

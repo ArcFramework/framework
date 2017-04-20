3b85b033ccee3f61309b4d61314b113f03c87502
Andrew Feeney <webspannerdev@gmail.com>
2017-04-20 21:29:11 +1000

    Add bootstrap class to register service providers

---
+++ src/Arc/Bootstrap/RegisterProviders.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Arc\Bootstrap;
+
+use Illuminate\Contracts\Foundation\Application;
+
+class RegisterProviders
+{
+    /**
+     * Bootstrap the given application.
+     *
+     * @param  \Illuminate\Contracts\Foundation\Application  $app
+     * @return void
+     */
+    public function bootstrap(Application $app)
+    {
+        $app->registerConfiguredProviders();
+    }
+}

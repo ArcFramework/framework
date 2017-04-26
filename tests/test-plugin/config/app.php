1e645e7bd781a8631c9676da41e8981b9e282ac3
Andrew Feeney <webspannerdev@gmail.com>
2017-04-27 06:31:22 +1000

    Add test plugin config boilerplate, including framework SPs

--- tests/test-plugin/config/app.php
+++ tests/test-plugin/config/app.php
@@ -1,5 +1,25 @@
 <?php
 
 return [
-    'providers' => [],
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
+    'providers' => [
+
+        /*
+         * Framework Service Providers...
+         */
+        Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Filesystem\FilesystemServiceProvider::class,
+        Illuminate\View\ViewServiceProvider::class,
+
+    ]
 ];

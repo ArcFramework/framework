67b845744117dbe4fe0869c64ac515d9a170585f
Andrew Feeney <webspannerdev@gmail.com>
2017-05-04 10:29:20 +1000

    Those pesky double forward slashes though

--- src/Arc/helpers.php
+++ src/Arc/helpers.php
@@ -1,2 +1,12 @@
 <?php
 
+/**
+ * Remove any double slashes from a URL
+ * @param string $string
+ * @return string
+ **/
+if (!function_exists('rds')) {
+    function rds($string) {
+        return preg_replace('#/+#', '/', $string);
+    }
+}

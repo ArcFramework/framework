eccf44c4e088a4cb55e97404f04c942abfa47256
Andrew Feeney <webspannerdev@gmail.com>
2017-05-24 14:43:23 +1000

    Add addMeta method

--- src/Arc/Models/User.php
+++ src/Arc/Models/User.php
@@ -53,6 +53,11 @@ class User extends Model
         ]);
     }
 
+    public function addMeta($key, $value, $unique = false)
+    {
+        return add_user_meta($this->ID, $key, $value, $unique);
+    }
+
     /**
      * Sets the given usermeta key to the given value if a key value pair is provided
      * or sets the key value pairs in the array if an array is provided as the first argument
@@ -76,6 +81,70 @@ class User extends Model
     public function findMeta($key, $single = true)
     {
         return get_user_meta($this->ID, $key, true);
+    }
+
+    /**
+     * Returns the PostMeta rows matching the given key in a Collection
+     *
+     * @param string $key The meta_key
+     * @return Illuminate\Support\Collection
+     **/
+    public function getMeta($key)
+    {
+        return $this->userMeta()
+            ->where('meta_key', $key)
+            ->get();
+    }
+
+    /**
+     * Deletes the all the usermeta for the user matching the given key or key and value
+     * if a value is provided
+     * @param string $key
+     * @param mixed $value (optional)
+     **/
+    public function deleteMeta($key, $value = null)
+    {
+        // dump($this->ID, $key, $value);
+        return delete_user_meta($this->ID, $key, $value);
+    }
+
+    /**
+     * A User has many UserMeta
+     **/
+    public function userMeta()
+    {
+        return $this->hasMany(UserMeta::class, 'user_id', 'ID');
+    }
+
+    /**
+     * Returns true if the user has a usermeta record matching the given key
+     * and value if provided
+     * @param string $key
+     * @param mixed $value (optional)
+     * @return bool
+     **/
+    public function hasMeta($key, $value = null)
+    {
+        $query = $this->userMeta()
+            ->where('meta_key', $key);
+
+        if ($value) {
+            $query = $query->where('meta_value', $value);
+        }
+
+        return !is_null($query->first());
+    }
+
+    public function addUniqueMeta($key, $value)
+    {
+        if (!is_null(UserMeta::where('user_id', $this->ID)
+            ->where('meta_key', $key)
+            ->where('meta_value', $value)
+            ->first())) {
+            return;
+        }
+
+        return $this->addMeta($key, $value);
     }
 }
 

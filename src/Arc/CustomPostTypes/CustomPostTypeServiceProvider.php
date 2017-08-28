9ac21a14d5b24af141284b2da06656d022d85be5
Andrew Feeney <webspannerdev@gmail.com>
2017-08-28 17:24:19 +1000

    Add service provider for custom post types

---
+++ src/Arc/CustomPostTypes/CustomPostTypeServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Arc\CustomPostType;
+
+use Illuminate\Support\ServiceProvider;
+
+class CustomPostTypeServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Register all custom post types
+        $registrar = $this->app->make('wordpress.custom_post_types');
+        $registrar->registerAll();
+    }
+
+    public function register()
+    {
+        // Bind the custom post types handler
+        $this->app->instance(
+            'wordpress.custom_post_types',
+            $this->app->make(CustomPostTypes::class)
+        );
+    }
+}
+
